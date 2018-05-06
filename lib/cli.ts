50082cb4279788665af748b1f1b551873e86f58a
Olegzandr VD <alex@oresoftware.com>
2018-05-06 15:55:11 -0700

    pdev:'empty commit message'

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 'use strict';
 
-console.log('humpty-dumpty has run!');
+console.log('humpty-dumpty has run! Current path:', __filename);
--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,3 @@
 'use strict';
 
-
-console.log('this is the humpty-dumpty library.');
+console.log('this is the humpty-dumpty library, at path:', __filename);
